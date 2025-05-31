d7ee09dcb96ad5b5b306b06ca1852cf55fa308fc
gariesbaries <aditiariski629@gmail.com>
2025-05-31 14:45:59 +0700

    finish

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+   static String nama = "gema";
+    static String gambar = "assets/mahasiswa/gema.jpeg";
 }
